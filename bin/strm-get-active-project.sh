5598fb221f60c2e54b7c0933f6fd38a65aaaf43d
Ivan Plantevin <ivan@strmprivacy.io>
2023-06-29 10:25:06 +0200

    cleanup

--- bin/strm-get-active-project.sh
+++ bin/strm-get-active-project.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-email=$(strm auth show | grep -oE '\[[^]]+\]' | tr -d '[]')
-echo "$email"
-
+active_user_email=$(strm auth show | grep -oE '\[[^]]+\]' | tr -d '[]')
 p=${STRM_CONFIG_PATH:-$HOME/.config/strmprivacy}
-active_projects=$(<$p/active_projects.json)
-
-active_project=$(cat $p/active_projects.json | jq -r '.users[] | select(.email == "'"$email"'") | .active_project')
+active_project=$(cat $p/active_projects.json | jq -r '.users[] | select(.email == "'"$active_user_email"'") | .active_project')
 strm list projects  -o json | jq --arg n "$active_project" -r '.projects[] | select(.name == $n)'
