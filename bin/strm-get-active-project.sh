4b6db59c07d9cc845ff44d17651a8f5bcdef9151
Robin Trietsch <robin.trietsch@gmail.com>
2024-04-10 13:11:11 +0200

    feat: cleanup and fix gl clone all script bug

--- bin/strm-get-active-project.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-active_user_email=$(strm auth show | grep -oE '\[[^]]+\]' | tr -d '[]')
-p=${STRM_CONFIG_PATH:-$HOME/.config/strmprivacy}
-active_project=$(cat $p/active_projects.json | jq -r '.users[] | select(.email == "'"$active_user_email"'") | .active_project')
-strm list projects  -o json | jq --arg n "$active_project" -r '.projects[] | select(.name == $n)'
